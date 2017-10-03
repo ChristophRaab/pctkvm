ee1d11512f92fc239198bc67b2d52752fd95c27a
Christoph Raab <c.raab@skalium.de>
2017-10-03 12:25:46 +0200

    Grammar check

--- Source Code/code/RT-PCVM/testGausRandn.m
+++ Source Code/code/RT-PCVM/testGausRandn.m
@@ -1,29 +1,29 @@
 % This script visualizes the problem of transfer learning with and synthetic
 % gaussian dataset
 
-close all;
-
-Z= [randn(100,3); randn(100,3)+2];
-Ys = [ones(100,1); ones(100,1)*2];
-
-X= [randn(100,3)-5; randn(100,3)-6];
-Yt = [ones(100,1)*1; ones(100,1)*2];
-
-D = [Z;X];
-Y = [Ys;Yt];
-color_1 = [1 1 0]; % yellow: source label1
-color_2 = [1 0 1];  %magenta: source label2
-color_3 = [0 1 0]; %green: target label3
-color_4 = [0 0 1]; %blue: target label4
-
-cmap = [color_1; color_2; color_3;color_4];
-label = cmap(Y,:);
-
-figure;
-scatter3(D(:,1),D(:,2),D(:,3),ones(size(Y))*25,label);
-title('Dataset with two Domains')
-
-Z = zscore(Z,1); X = zscore(X,1);
+% close all;
+% 
+% Z= [randn(100,3); randn(100,3)+2];
+% Ys = [ones(100,1); ones(100,1)*2];
+% 
+% X= [randn(100,3)-5; randn(100,3)-6];
+% Yt = [ones(100,1)*1; ones(100,1)*2];
+% 
+% D = [Z;X];
+% Y = [Ys;Yt];
+% color_1 = [1 1 0]; % yellow: source label1
+% color_2 = [1 0 1];  %magenta: source label2
+% color_3 = [0 1 0]; %green: target label3
+% color_4 = [0 0 1]; %blue: target label4
+% 
+% cmap = [color_1; color_2; color_3;color_4];
+% label = cmap(Y,:);
+% 
+% figure;
+% scatter3(D(:,1),D(:,2),D(:,3),ones(size(Y))*25,label);
+% title('Dataset with two Domains')
+% 
+% Z = zscore(Z,1); X = zscore(X,1);
 
 % Z =  tsne(Z,'Algorithm','barneshut','Distance','euclidean','NumDimensions',2);
 % X =  tsne(X,'Algorithm','barneshut','Distance','euclidean','NumDimensions',2);
@@ -103,12 +103,12 @@ color_2 = [1 0 1];  %magenta: source label2
 color_3 = [0 1 0]; %green: target label1
 color_4 = [0 0 1]; %blue: target label2
 
-cmap = [color_1; color_2;];
+cmap = [color_3; color_4;];
 label = cmap(Y,:);
 
 figure;
 scatter3(D(:,1),D(:,2),D(:,3),ones(size(Y))*25,label);
-title('Dataset with two Domains')
+%title('Dataset with two Domains')
 
 Z = zscore(Z,1); X = zscore(X,1);
 D = [Z;X];
@@ -124,5 +124,5 @@ Z = U*ZS*V';
 D = [Z;X];
 figure;
 scatter3(D(:,1),D(:,2),D(:,3),ones(size(Y))*25,label);
-title('Rotation with two Domains')
+%title('Rotation with two Domains')
 
--- Source Code/code/visualizeResult.m
+++ Source Code/code/visualizeResult.m
@@ -76,6 +76,21 @@ legend('PCTKVM','PCTKVMest','TCA','JDA','GFK');
 xlim([0 13])
 hold off;
 
+load ../result/fivetwo_image_Result.mat;
+mea = meanErrors'; mstd = stdErrors';
+figure;
+errorbar(mea(end,:),mstd(end,:))
+hold on;
+errorbar(mea(end-1,:),mstd(end-1,:))
+errorbar(mea(2,:),mstd(2,:));
+errorbar(mea(1,:),mstd(1,:))
+errorbar(mea(6,:),mstd(6,:))
+ylabel('Error in %')
+xlabel('No. Image Dataset')
+legend('PCTKVM','PCTKVMest','PCVM','SVM','TKL');
+xlim([0 13])
+hold off;
+
 load ../result/fivetwo_reuters_Result.mat;
 mea = meanErrors'; mstd = stdErrors';
 figure;
@@ -88,5 +103,20 @@ errorbar(mea(5,:),mstd(5,:))
 ylabel('Error in %')
 xlabel('No. Reuters Dataset')
 legend('PCTKVM','PCTKVMest','TCA','JDA','GFK');
+xlim([0 7])
+hold off;
+
+load ../result/fivetwo_reuters_Result.mat;
+mea = meanErrors'; mstd = stdErrors';
+figure;
+errorbar(mea(end,:),mstd(end,:))
+hold on;
+errorbar(mea(end-1,:),mstd(end-1,:))
+errorbar(mea(2,:),mstd(2,:));
+errorbar(mea(1,:),mstd(1,:))
+errorbar(mea(6,:),mstd(6,:))
+ylabel('Error in %')
+xlabel('No. Reuters Dataset')
+legend('PCTKVM','PCTKVMest','PCVM','SVM','TKL');
 xlim([0 7])
 hold off;
